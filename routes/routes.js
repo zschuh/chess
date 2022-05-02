054d34a56b00277ce80e4ce9bdf31dfee3982abc
xaviles <55527999+xaviles@users.noreply.github.com>
2022-05-02 15:50:49 -0400

    updated routes

---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+// require database functions once implemented
+
+// '/' route == homepage
+router.get('/', async (req, res) => {
+    res.render('general/frontPage');
+});
+
+router.get('/user', async (req, res) => {
+    res.render('general/user');
+});
+
+// plan on having this route be called by ajax call
+// in order to relay user info from database, then jquery the page with that info
+router.post('/user', async (req, res) => {
+
+});
+
+router.get('/game', async (req, res) => {
+    res.render('general/game');
+});
+
+router.get('/leaderboard', async (req, res) => {
+    res.render('general/leaderboard');
+});
+
+router.get('/results', async (req, res) => {
+    res.render('general/results');
+});
+
+module.exports = router;
