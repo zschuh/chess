732a349324c0a882cd8a43f1acac6a8dbee0783a
xaviles <55527999+xaviles@users.noreply.github.com>
2022-05-08 14:54:27 -0400

    input length validation + remove gameResults page

--- data/validation.js
+++ data/validation.js
@@ -1,6 +1,7 @@
 function checkEmail(email) {
     if (!email) { throw 'Email must be given' }
     if (typeof email !== 'string') { throw 'Email must be of type string' }
+    if (email.length > 50) { throw 'Email must not exceed 50 characters' }
     //Regex to validate email format of x@x.x
     if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) { throw 'Not a valid email' }
 }
@@ -9,17 +10,18 @@ function checkUsername(username) {
     if (!username) { throw 'Username must be supplied' }
     if (typeof username !== 'string') { throw 'Username must be of type string' }
     if (username.length === 0) { throw 'Username must not be empty' }
+    if (username.length < 4 || username.length < 25) { throw 'Username must be between 4 and 25 characters long' }
     //Regex for alphanumeric
     if (!/^[a-zA-Z0-9]*$/.test(username)) { throw 'Username can only be made up of alphanumeric characters a-z and 0-9' }
-    if (username.length < 4) { throw 'Username must be at least 4 characters long' }
 }
 
 function checkPassword(password) {
     if (!password) { throw 'Password must be supplied' }
     if (typeof password !== 'string') { throw 'Password must be of type string' }
     if (password.length === 0) { throw 'Password must not be empty' }
+    if (password.length < 6 || password.length > 30) { throw 'Password must be between 6 and 30 characters long' }
+    //Regex for password
     if (!/^[^\s]*$/.test(password)) { throw 'Password can not contain any spaces' }
-    if (password.length < 6) { throw 'Password must be at least 6 characters long' }
 }
 
 function checkResult(result){
